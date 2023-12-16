8c8513ddabc29ea6e240191ecf91173202f5486c
Jian Liao <jianliao@adobe.com>
2023-12-16 08:25:29 -0800

    Optimize ffmpeg process logic

--- video_subs/ffmpeg_utils.py
+++ video_subs/ffmpeg_utils.py
@@ -24,54 +24,50 @@ def process_input_video(input_path, subtitles_file, font_name='Adobe Clean Han',
         if os.path.exists(output_path):
             os.remove(output_path)
 
-        # Get the dimensions of the video
-        video_width, video_height = get_video_dimensions(input_path)
-
-        # Base ffmpeg command
+        # Initialize command and filters
         cmd = ['ffmpeg', '-i', input_path]
-
-        # Construct the complex filter string
         complex_filters = []
 
-        # Add scaling if specified
+        # Check if resizing is needed
         if width is not None and height is not None:
-            scale_filter = f'scale={width}:{height}'
-            complex_filters.append(scale_filter)
-            video_width = width
-            video_height = height
+            complex_filters.append(f'scale={width}:{height}')
 
-        # Add blur filter if blur_area is specified
+        # Check if blur filter is needed
         if blur_area is not None:
+            video_width, video_height = get_video_dimensions(input_path)
             x, y, blur_width, blur_height = blur_area
             x = video_width - x  # Position the blur area at the bottom right corner
             y = video_height - y
             blur_filter = f'split=2[base][blur];[blur]crop={blur_width}:{blur_height}:{x}:{y},boxblur=luma_radius=min(h\\,w)/20:luma_power=1[blurred];[base][blurred]overlay={x}:{y}'
             complex_filters.append(blur_filter)
 
-        # Combine complex filters
-        if complex_filters:
-            cmd.extend(['-filter_complex', ';'.join(complex_filters)])
-
         # Encoding settings for output quality
         if output_quality == 'high':
-            cmd.extend(['-crf', '10', '-preset', 'slower'])
+            quality_settings = ['-crf', '10', '-preset', 'slower']
         else:
-            cmd.extend(['-crf', '23', '-preset', 'medium'])
+            quality_settings = ['-crf', '23', '-preset', 'medium']
 
-        # Output file for intermediate step
-        intermediate_output = os.path.splitext(input_path)[0] + '_temp.mp4'
-        cmd.extend([intermediate_output])
+        # Apply complex filters if needed
+        if complex_filters:
+            cmd.extend(['-filter_complex', ';'.join(complex_filters)])
+            intermediate_output = os.path.splitext(input_path)[0] + '_temp.mp4'
+            cmd.extend(quality_settings + [intermediate_output])
 
-        # Execute the first part of the command
-        subprocess.run(cmd, check=True)
+            # Execute the first part of the command
+            subprocess.run(cmd, check=True)
 
-        # Apply subtitles in a second step
-        subtitles_cmd = ['ffmpeg', '-i', intermediate_output, '-vf',
-                         f"subtitles={subtitles_file}:force_style='FontName={font_name},FontSize={font_size},Shadow=0,BackColour=&H80000000,BorderStyle=4'", output_path]
-        subprocess.run(subtitles_cmd, check=True)
+            # Apply subtitles in a second step
+            subtitles_cmd = ['ffmpeg', '-i', intermediate_output, '-vf',
+                             f"subtitles={subtitles_file}:force_style='FontName={font_name},FontSize={font_size},Shadow=0,BackColour=&H80000000,BorderStyle=4'", output_path]
+            subprocess.run(subtitles_cmd, check=True)
 
-        # Remove the intermediate file
-        os.remove(intermediate_output)
+            # Remove the intermediate file
+            os.remove(intermediate_output)
+        else:
+            # Only apply subtitles if no resizing or blur is needed
+            cmd.extend(
+                ['-vf', f"subtitles={subtitles_file}:force_style='FontName={font_name},FontSize={font_size},Shadow=0,BackColour=&H80000000,BorderStyle=4'"] + quality_settings + [output_path])
+            subprocess.run(cmd, check=True)
 
         return True
     except Exception as e:
@@ -80,7 +76,7 @@ def process_input_video(input_path, subtitles_file, font_name='Adobe Clean Han',
 
 
 # Test the function
-# result = process_input_video(input_path='/Users/jianliao/Desktop/SocialNetwork/18_KRON4/input.mov',
-#                        subtitles_file='/Users/jianliao/Desktop/SocialNetwork/18_KRON4/cn.srt', blur_area=(185, 182, 105, 105))
+# result = process_input_video(input_path='/Users/jianliao/Desktop/SocialNetwork/47_KRON4/input.mp4',
+#                        subtitles_file='/Users/jianliao/Desktop/SocialNetwork/47_KRON4/cn.srt', blur_area=(185, 182, 105, 105))
 
 # print('Processing result:', result)
